19112c3def7193d19f5668cd24ad19de9ed453d3
Nguyen Huu Tam <lkyxmen@gmail.com>
2016-01-02 18:52:34 +0700

    Create version.php

---
+++ cnt-cpanel/version.php
@@ -0,0 +1,17 @@
+<?php
+
+/*-----------------------------------*\
+|           Copyright © CNT           | 
+|         Phone: 0986.901.797         |
+|         Y!m: banmai_xanhmai         |
+|       Website: CongNgheTre.Vn       |
+|     Email: PeakOfMusic@Gmail.Com    |
+\*-----------------------------------*/
+
+$version = array(
+    'name'  => '',
+    'value' => '1.0',
+    'time'  => '1234567890',
+);
+
+?>
