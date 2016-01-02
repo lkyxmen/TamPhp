e382e8600625ea69b0e6b7c617ec015dfdcca565
Nguyen Huu Tam <lkyxmen@gmail.com>
2016-01-02 17:50:28 +0700

    Create cnt-logout.php

---
+++ cnt-logout.php
@@ -0,0 +1,14 @@
+<?php
+
+/*-----------------------------------*\
+|           Copyright © CNT           | 
+|         Phone: 0986.901.797         |
+|         Y!m: banmai_xanhmai         |
+|       Website: CongNgheTre.Vn       |
+|     Email: PeakOfMusic@Gmail.Com    |
+\*-----------------------------------*/
+
+if (!session_id()) session_start();
+unset($_SESSION['user']);
+header('Location: home.html');
+?>
