f2dcdc3e8d7a2256bd9458a09d33674209c2617b
Nguyen Huu Tam <lkyxmen@gmail.com>
2016-01-02 17:56:11 +0700

    Create cnt-security.php

---
+++ cnt-security.php
@@ -0,0 +1,13 @@
+<?php
+
+/*-----------------------------------*\
+|           Copyright © CNT           | 
+|         Phone: 0986.901.797         |
+|         Y!m: banmai_xanhmai         |
+|       Website: CongNgheTre.Vn       |
+|     Email: PeakOfMusic@Gmail.Com    |
+\*-----------------------------------*/
+
+
+
+?>
